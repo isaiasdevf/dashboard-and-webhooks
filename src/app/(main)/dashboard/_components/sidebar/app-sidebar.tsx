6fe11bc69b8b945136f556f1c48d195cfaac34ec
isaiasdevf <161076921+isaiasdevf@users.noreply.github.com>
2025-06-02 19:53:01 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/(main)/dashboard/_components/sidebar/app-sidebar.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import {
+  IconDatabase,
+  IconFileWord,
+  IconHelp,
+  IconInnerShadowTop,
+  IconReport,
+  IconSearch,
+  IconSettings,
+} from "@tabler/icons-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { APP_CONFIG } from "@/config/app-config";
+import { rootUser } from "@/data/users";
+import { sidebarItems } from "@/navigation/sidebar/sidebar-items";
+
+import { NavMain } from "./nav-main";
+import { NavUser } from "./nav-user";
+
+const data = {
+  navSecondary: [
+    {
+      title: "Settings",
+      url: "#",
+      icon: IconSettings,
+    },
+    {
+      title: "Get Help",
+      url: "#",
+      icon: IconHelp,
+    },
+    {
+      title: "Search",
+      url: "#",
+      icon: IconSearch,
+    },
+  ],
+  documents: [
+    {
+      name: "Data Library",
+      url: "#",
+      icon: IconDatabase,
+    },
+    {
+      name: "Reports",
+      url: "#",
+      icon: IconReport,
+    },
+    {
+      name: "Word Assistant",
+      url: "#",
+      icon: IconFileWord,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild className="data-[slot=sidebar-menu-button]:!p-1.5">
+              <a href="#">
+                <IconInnerShadowTop className="!size-5" />
+                <span className="text-base font-semibold">{APP_CONFIG.name}</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={sidebarItems} />
+        {/* <NavDocuments items={data.documents} /> */}
+        {/* <NavSecondary items={data.navSecondary} className="mt-auto" /> */}
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={rootUser} />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
