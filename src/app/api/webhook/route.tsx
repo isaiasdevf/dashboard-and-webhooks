9f3aa1233c3d3f42d19068bcdb589cbf80211c56
isaiasdev <f.isaias2021@gmail.com>
2025-06-04 13:29:36 -0400

    url webhook

---
+++ src/app/api/webhook/route.tsx
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  console.log("Webhook recibido:", body);
+
+  // Aquí puedes hacer lo que necesites con los datos
+
+  return NextResponse.json({ message: "Webhook recibido con éxito" });
+}
