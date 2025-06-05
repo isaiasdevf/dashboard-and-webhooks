eb447d77230c60bf90262df7cc21b90f288471df
isaiasdev <f.isaias2021@gmail.com>
2025-06-05 16:30:30 -0400

    update console

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
+  // compiler: {
+  //   removeConsole: process.env.NODE_ENV === "production",
+  // },
   devIndicators: false,
   async redirects() {
     return [
